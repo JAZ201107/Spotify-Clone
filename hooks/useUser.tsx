c2b93b6b02222227f551195111eca115e4386201
zhang yuyang <zhangyuyang1211@gmail.com>
2023-08-02 22:07:01 +0800

    Authentication Finished

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -65,6 +65,7 @@ export const MyUserContextProvider = (props: Props) => {
       setUserDetails(null);
       setSubscription(null);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, isLoadingUser]);
 
   const value = {
